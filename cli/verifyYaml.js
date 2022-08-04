61e71f88d1347a167a825fafde4ef899743c805f
Joe Cyboski <joe.cyboski@nytimes.com>
2022-08-04 13:57:02 -0400

    added support for yml file extension. Added debug logging of file body

--- cli/verifyYaml.js
+++ cli/verifyYaml.js
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 var glob = require("glob");
 var fs = require("fs");
+var util = require("util");
 var yaml = require("js-yaml");
+var debug = require("debug")
+
+var log = debug("yaml");
+var body = debug("body");
+
+var bodyDepth = process.env["BODY_DEPTH"] || 1;
 
 function verifyFile(file){
+  console.log("Verify file", file);
   fs.readFile(file, "utf8", function(err, buff){
     try{
       if(err) throw err;
       var data = yaml.safeLoad(buff);
+      body("File %s, body %s", file, util.inspect(data, false, bodyDepth, true));
 
     }catch(err){
      console.log("ERROR: " + file + "\n" + err.message);
@@ -17,8 +26,15 @@ function verifyFile(file){
 
 glob("**/*.yaml", function(err, files){
   if(err) throw err;
+  log("files = ", files);
 
   files.forEach(verifyFile);
 });
 
+glob("**/*.yml", function(err, files){
+  if(err) throw err;
+  log("files = ", files);
+
+  files.forEach(verifyFile);
+});
 
